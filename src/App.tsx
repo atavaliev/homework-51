a1ecf8c233c6a6264065c900c5e5bfd16e427c1e
Atavaliev Timur <tatavaliev@gmail.com>
2023-05-16 13:50:45 +0600

    Create generateNumbers function, sort number's list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Circle from "./Circle/Circle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [numbers, setNumbers] = useState<number[]>([0, 0, 0, 0, 0]);
+
+    const generateNumbers = () => {
+
+        //Create collection of values:number
+        const randomNumbers: Set<number> = new Set();
+
+        //Generate unique numbers till array size reach 5
+        for (let i = 0; randomNumbers.size < 5; i++) {
+            const randomNumber: number = Math.floor(Math.random() * 36) + 5;
+            randomNumbers.add(randomNumber);
+        }
+
+        //Convert Set to Array
+        setNumbers(Array.from(randomNumbers));
+    }
+
+    //Sorted array to ascendant
+    const sortedArray = numbers.sort(function (a, b) {
+        return a - b
+    });
+
+    return (
+        <div className="App">
+            <div className="circle-list">
+                {
+                    sortedArray.map(n => {
+                        return <Circle number={n}/>
+                    })
+                }
+            </div>
+
+            <button onClick={() => generateNumbers()}>New numbers</button>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Circle/Circle.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Circle.css'
+
+
+interface PropsCircle {
+    number:number
+}
+
+const Circle:React.FC<PropsCircle> = ({number}) => {
+    return (
+        <div className="circle">
+            {number}
+        </div>
+    );
+};
+
+export default Circle;
